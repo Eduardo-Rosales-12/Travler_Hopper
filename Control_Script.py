420b97720e7b10f79359fd5bfdfeec2f7703ab67
Eduardo-Rosales-12 <103926234+Eduardo-Rosales-12@users.noreply.github.com>
2024-11-08 13:49:21 -0800

    Update Control_Script.py
    Removed USB communication portions

--- Control_Script.py
+++ Control_Script.py
@@ -104,180 +104,27 @@ def get_Torques(Jacobian, Forces):
     return Torques 
 
 
-def get_encoder_pos_CAN(can_bus, node_id):
-    # Request Encoder Position
-    pos_msg = can.Message(arbitration_id=0x009 | (node_id << 5), is_extended_id=False)
-    can_bus.send(pos_msg)
-    
-    # Wait for response and process position
-    position_response = can_bus.recv(0.001)  # Wait for 1 ms to receive response --> this might be to slow so this should be tested 
-    if position_response is not None:
-        # Unpack the 4-byte float from the data
-        encoder_pos = (struct.unpack('<f', position_response.data[0:4])[0])/360
-        print(f"Encoder Position: {encoder_pos} turns")
-        return encoder_pos
-    else:
-        return "No Response :("
-    
-def  get_encoder_vel_CAN(can_bus, node_id):
-    # Request Encoder Velocity
-    vel_msg = can.Message(arbitration_id=0x00A  | (node_id << 5), is_extended_id=False)
-    can_bus.send(vel_msg)
-    
-    # Wait for response and process velocity
-    velocity_response = can_bus.recv(0.001)  # Wait for 1 ms to receive response --> this might be to slow so this should be tested 
-    if velocity_response is not None:
-        # Unpack the 4-byte float from the data
-        encoder_vel = (struct.unpack('<f', velocity_response.data[0:4])[0])/360
-        print(f"Encoder Velocity: {encoder_vel} turns/second")
-        return encoder_vel
-    else:
-        return "No Response :("
-
-def get_encoder_values_USB(odrv_object):
-    encoder_pos = (odrv_object.axis0.encoder.pos_estimate)/360 #degrees
-    encoder_vel = (odrv_object.axis0.encoder.vel_estimate)/360 #degrees/s
-
-    return encoder_pos, encoder_vel
-
-def update_plot(x, z):
-    x_data.append(x)
-    z_data.append(z)
-    line.set_xdata(x_data)
-    line.set_ydata(z_data)
-    ax.relim()   # Recompute the limits based on the new data
-    ax.autoscale_view()  # Automatically adjust the plot view
-    plt.draw()
-    plt.pause(0.001)  # Pause to allow for the plot to update
 
 if __name__ == "__main__":
-    # Create an argument parser
-    parser = argparse.ArgumentParser(description="Travler Hopper Operation.")
 
-    # Add arguments
-    parser.add_argument("--mode", type=float,choices=['Hopping', 'Dropping', 'Dynamic Probing'], default="Hopping")
-
-    # Parse the arguments
-    args = parser.parse_args()
-
-    #Set up CAN interface 
-    CAN_Interface = 'can0'
-
-    #ODrive CAN ID 
-    Odrive_CAN_ID = 0x01
-
-    #Initialize Can Bus Object 
-    can_bus = can.interface.BUS(channel=CAN_Interface, bustype = 'socketcan')
-
-    #Initialize phase object 
-    Motion_Phase = Hopper_State_Machine(Kc=1, Cd=1)
-
-    #Define serial number of each odrive being used 
-    Odrv0_Serial = "XXXXXXXXXXXXXXXX" #Change to correct serial number 
-    Odrv1_Serial = "XXXXXXXXXXXXXXXX" #Change to correct serial number 
-
-
-    #Connect to odrive 
-    odrv0 = odrive.find_any(Odrv0_Serial)
-    odrv1 = odrive.find_any(Odrv1_Serial)
-
-    #Calibrate Motor 1 and wait for it to complete calibration
-    odrv0.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
-    while odrv0.axis0.current_state != AXIS_STATE_IDLE:
-        time.sleep(0.1)
-
-    #Calibrate Motor 2 and wait for it complete calibration
-    odrv1.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
-    while odrv1.axis0.current_state != AXIS_STATE_IDLE:
-        time.sleep(0.1)
-
-    #Establish closed loop control and set up torque control 
-    odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-    odrv0.axis0.controller.config.control_mode = CTRL_MODE_POSITION_CONTROL
-    odrv1.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-    odrv1.axis0.controller.config.control_mode = CTRL_MODE_POSITION_CONTROL
-
-    #Define key variables
-    idle_latch = 0
-    dt = 0.001
-
-    #Set up live plotter to test control scheme 
-    # Set up Matplotlib for live plotting
-    plt.ion()  # Enable interactive mode
-    fig, ax = plt.subplots()
-    x_data, z_data = [], []
-    line, = ax.plot([], [], 'r-')
-    ax.set_xlim(-100, 100)  # Adjust as per the workspace of the hopper
-    ax.set_ylim(0, 300)     # Adjust to accommodate the maximum height of the hopper
-    ax.set_xlabel('X Position (mm)')
-    ax.set_ylabel('Z Position (mm)')
-    ax.set_title('Hopper Position During Motion')
 
     if args.mode == "Hopping":
         while True:
-            Prev_Motor1_Pos = get_encoder_pos_CAN(can_bus, 1)
-            Prev_Motor2_Pos = get_encoder_pos_CAN(can_bus, 2)
-            Prev_Leg_Geometry = get_leg_geometry(Prev_Motor1_Pos[0], Prev_Motor2_Pos[0])
-
-            #Wait 1 ms before collecting the next data point
-            time.sleep(dt) #this might be to slow so this should be tested 
-
-            #Recalculate centerbar linkage length 
-            Curr_Motor1_Pos = get_encoder_pos_CAN(can_bus, 1)
-            Curr_Motor2_Pos = get_encoder_pos_CAN(can_bus, 2)
-            Curr_Leg_Geometry= get_leg_geometry(Curr_Motor1_Pos[0], Curr_Motor2_Pos[0])
-           
-            #update plot 
-            update_plot(Curr_Coords[0], Curr_Coords[1])
-            
-            #Calculate Jacobian 
-            Jacobian = get_Jacobian(Curr_Motor1_Pos, Curr_Motor2_Pos)
-
-            #Calculate the change in length of the centerbar linkage overtime 
-            Centerbar_Deriv = (Curr_Leg_Geometry[0] - Prev_Leg_Geometry[0])/(dt*4) # the value that is divided is the amount of time it takes for the get_encoder_values_CAN() function 
-                                                                # to execute 4 times as it is called 4 times before this line plus te additional time it takes for 1ms to pass 
-                                                                # between the first and second ecnoder measurement. I would love suggestions on how to improve this :/
-            #Calulcate Change in the position of robots body with respect the joint frame of refrence 
-            Prev_Coords = FK(Prev_Leg_Geometry["Leg Length"], Prev_Leg_Geometry["Leg Angle"])
-            Curr_Coords= FK(Curr_Leg_Geometry["Leg Length"], Curr_Leg_Geometry["Leg Angle"])
             
             if keyboard.is_pressed('a'):
-                idle_latch = 1
+
 
             if Motion_Phase.get_state(Centerbar_Deriv, idle_latch) == "idle":
-                #The goal here is to initiate the compresion phase by applying small torques at the motors to shrink the length of the centerbar linkage 
-                if keyboard.is_pressed('b'):
-                    odrv0.axis0.controller.input_torque = 0.10 #Nm
-                    odrv1.axis0.controller.input_torque = -0.10 #Nm
+
 
             elif Motion_Phase.get_state(Centerbar_Deriv, idle_latch) == "compression":
-                Motion_Phase.compression()
 
-                Force_PD_Controller = PDController(Kp=Motion_Phase.K_c, Kd=Motion_Phase.Cd)
-                Forces = np.array([0,Force_PD_Controller.update(Curr_Coords[1], Prev_Coords[1], dt)])
-                Torques = get_Torques(Jacobian, Forces)
-
-                odrv0.axis0.controller.input_torque = Torques[0]
-                odrv1.axis0.controller.input_torque = Torques[1]
 
             elif Motion_Phase.get_state(Centerbar_Deriv, idle_latch) == "extension":
-                Motion_Phase.extension()
 
-                Force_PD_Controller = PDController(Kp=Motion_Phase.K_c, Kd=Motion_Phase.Cd)
-                Forces = np.array([0,Force_PD_Controller.update(Curr_Coords[1], Prev_Coords[1], dt)])
-                Torques = get_Torques(Jacobian, Forces)
-
-                odrv0.axis0.controller.input_torque = Torques[0]
-                odrv1.axis0.controller.input_torque = Torques[1]
 
             elif Motion_Phase.get_state(Centerbar_Deriv, idle_latch) == "flight":
-                Motion_Phase.flight()
-                Flight_Positon_Controller = PDController(Kp=0.5, Kd=0.5)
-                Motor1_Position_Control_Signal = Flight_Positon_Controller.update(0,Curr_Motor1_Pos,dt)
-                Motor2_Position_Control_Signal = Flight_Positon_Controller.update(0,Curr_Motor1_Pos,dt)
 
-                odrv0.axis0.controller.input_pos = Motor1_Position_Control_Signal
-                odrv1.axis0.controller.input_pos = Motor2_Position_Control_Signal
 
             else:
                 print("Phase state could not be determined.")
@@ -294,5 +141,3 @@ if __name__ == "__main__":
         while True: 
             print("I'm probing some more ;)")
 
-plt.ioff()
-plt.show()
