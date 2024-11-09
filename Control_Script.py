67c9069115e0c50f3ff895c065f6b43915743fb2
Eduardo-Rosales-12 <103926234+Eduardo-Rosales-12@users.noreply.github.com>
2024-11-08 18:06:57 -0800

    Update Control_Script.py
    Altered switch conditions to rely directly on encoder readings

--- Control_Script.py
+++ Control_Script.py
@@ -24,7 +24,7 @@ class PDController:
 
 
 
-class Hopper_State_Machine():
+class Hopper_State_Machine:
     def __init__(self, Kc, Cd):
         self.Kc = Kc
         self.Cd = Cd 
@@ -44,18 +44,23 @@ class Hopper_State_Machine():
         print("To Infinity and Beyond")
         
     
-    def get_state(self, len_deriv, latch_status):
-
+    def get_state(self, motor1_encoder_estimate, motor2_encoder_estimate, latch_status):
+        extension_limit_value = 0.3
+        compression_limit_value = 0.1
+        
         if latch_status == 0: 
             return "idle"
         
         elif latch_status == 1:
-            if len_deriv >= -0.5 & len_deriv <= 0.5: #Here I am assuming that if the lenght of the centerbar linkage is changing by 0.5mm this is just noise introduced by the encoder 
-                return "idle/flight"                 #Should I rewrite the idle/flight statement in terms of reaction force felt on the ground 
+            if motor1_encoder_estimate >= extension_limit_value & motor2_encoder_estimate >= extension_limit_value: 
+                return "idle/flight"       
+                
             elif len_deriv < -0.5:
                 return "compression"
-            elif len_deriv > 0.5:
+                
+            elif motor1_encoder_estimate > compression_limit_value & motor2_encoder_estimate > compression_limit_value:
                 return "extension"
+                
             else:
                 return "State could not be determined!"
 
@@ -103,27 +108,142 @@ def get_Torques(Jacobian, Forces):
 
     return Torques 
 
+# Function to put a node into closed loop control mode
+def set_closed_loop_control(node_id):
+    bus.send(can.Message(
+        arbitration_id=(node_id << 5 | 0x07),  # 0x07: Set_Axis_State
+        data=struct.pack('<I', 8),  # 8: AxisState.CLOSED_LOOP_CONTROL
+        is_extended_id=False
+    ))
 
+# Function to put a node into closed loop control mode
+def set_torque_control_mode(node_id):
+    bus.send(can.Message(
+        arbitration_id=(node_id << 5 | 0x0b),  # 0x0b: Set_Controller_Mode
+        data=struct.pack('<II', 1, 1),  # 1: Torque control, 1:Input pass through mode
+        is_extended_id=False
+    ))
+
+def set_position_control_mode(node_id):
+    bus.send(can.Message(
+        arbitration_id=(node_id << 5 | 0x0b),  # 0x0b: Set_Controller_Mode
+        data=struct.pack('<II', 3, 1),  # 3: Position Control, 1: Input pass through mode
+        is_extended_id=False
+    ))
+
+# Function to set a torque command for a node
+def set_torque(node_id, torque):
+    bus.send(can.Message(
+        arbitration_id=(node_id << 5 | 0x0e),  # 0x0e: Set_Input_Torque
+        data=struct.pack('<f', torque),  # Set desired torque
+        is_extended_id=False
+    ))
+
+def set_position(node_id, position, ff_vel, ff_tor):
+    bus.send(can.Message(
+        arbitration_id=(node_id << 5 | 0x0c),
+        data=struct.pack('<fhh', position, int(ff_vel), int(ff_tor)),
+        is_extended_id=False
+    ))
+
+    
+def set_idle(node_id):
+    bus.send(can.Message(
+        arbitration_id=(node_id << 5 | 0x07),
+        data=struct.pack('<I', 1),
+        is_extended_id=False
+    ))
+
+def get_pos_estimate(node_id):
+    
+    bus.send(can.Message(
+        arbitration_id=(node_id << 5 | 0x09), # 0x04: RxSdo
+        data=b'',
+        is_extended_id=False
+    ))
+    
+    # Await reply
+    for msg in bus:
+        if msg.arbitration_id == (node_id << 5 | 0x09): # 0x05: TxSdo
+            break
+    # Unpack and print reply
+    _, _, _, pos_return_value = struct.unpack_from('<BHB' + 'f', msg.data)
+    pos_return_value = pos_return_value*(math.pi*2)
+    
+    return pos_return_value
+    
+
+
+def get_torque_estimate(node_id):
+
+    bus.send(can.Message(
+        arbitration_id=(node_id << 5 | 0x1c), 
+        data=b'',
+        is_extended_id=False
+    ))
+    
+    #Await reply
+    for msg in bus:
+        if msg.arbitration_id == (node_id << 5 | 0x1c): 
+            break
+        
+    # Unpack and print reply
+    _, _, _, torque_return_value = struct.unpack_from('<BHB' + 'f', msg.data)
+    
+    return torque_return_value
 
 if __name__ == "__main__":
 
+    nodes = [0, 1]  # Node IDs for node 0 and node 1
+    
+    bus = can.interface.Bus("can0", interface="socketcan")
+    
+    # Flush CAN RX buffer so there are no more old pending messages
+    while not (bus.recv(timeout=0) is None): 
+        pass
+    
+    # Put each node into closed loop control mode
+    for node_id in nodes:
+        set_closed_loop_control(node_id)
+        set_position_control_mode(node_id)
 
+    
+    # Wait for each node to enter closed loop control by scanning heartbeat messages
+    for node_id in nodes:
+        for msg in bus:
+            if msg.arbitration_id == (node_id << 5 | 0x01):  # 0x01: Heartbeat
+                error, state, result, traj_done = struct.unpack('<IBBB', bytes(msg.data[:7]))
+                if state == 8:  # 8: AxisState.CLOSED_LOOP_CONTROL
+                    break
+
+    #Initalize stat object 
+    State_Machine = Hopper_State_Machine(0,0)
+
+    #Initalize the latch status 
+    latch_status = 0
+    #Check the hopping mode
     if args.mode == "Hopping":
+        initial_motor1_pos = get_pos_estimate(nodes[0])
+        initial_motor2_pos =get_pos_estimate(nodes[1])
+        
         while True:
+            #grab initial motor position estimate 
+            motor1_pos = get_pos_estimate(nodes[0])
+            motor2_pos = get_pos_estimate(nodes[1])
             
-            if keyboard.is_pressed('a'):
+            #Check the system state
+            State =  State_Machine.get_state(motor1_pos, motor2_pos, latch_status)
+            
+            if State == "idle":
+                latch_status = 1
+
+            elif State == "compression":
 
 
-            if Motion_Phase.get_state(Centerbar_Deriv, idle_latch) == "idle":
+            elif State == "extension":
 
 
-            elif Motion_Phase.get_state(Centerbar_Deriv, idle_latch) == "compression":
-
-
-            elif Motion_Phase.get_state(Centerbar_Deriv, idle_latch) == "extension":
-
-
-            elif Motion_Phase.get_state(Centerbar_Deriv, idle_latch) == "flight":
+            elif State == "flight":
 
 
             else:
